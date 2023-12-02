30eb6a075bb9f5e6a43257e8a09272007e698b68
francium-96 <johntherippper96@gmail.com>
2023-12-02 18:10:16 +0000

    titan: Adding developer GSI keys and added fstab

--- device.mk
+++ device.mk
@@ -17,6 +17,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
