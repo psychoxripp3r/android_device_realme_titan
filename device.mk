524a8ba317d83a4b29fa9ded2b4a4ad402acff95
francium-96 <johntherippper96@gmail.com>
2023-12-02 17:33:13 +0000

    titan : Configure dynamic partitions

--- device.mk
+++ device.mk
@@ -16,6 +16,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_BUILD_SUPER_PARTITION := false
+
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
