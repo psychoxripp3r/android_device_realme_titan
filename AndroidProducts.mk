855cca5dd72d0a46588062a31342df0be303f136
francium-96 <johntherippper96@gmail.com>
2023-12-02 14:49:58 +0000

    skeleton tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_titan.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_titan-user \
+    lineage_titan-userdebug \
+    lineage_titan-eng
