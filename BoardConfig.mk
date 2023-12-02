9fd34973c3da9113299de5d3b6c447a76f6f75e4
francium-96 <johntherippper96@gmail.com>
2023-12-02 14:54:05 +0000

    titan: Define Architecture flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/oplus/ossi
+DEVICE_PATH := device/realme/titan
 
 # A/B
 AB_OTA_UPDATER := true
@@ -20,7 +20,7 @@ BOARD_USES_RECOVERY_AS_BOOT := true
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 := 
 TARGET_CPU_VARIANT := generic
