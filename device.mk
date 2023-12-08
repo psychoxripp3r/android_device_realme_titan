ee47a29be7f4ba1186a58954246c66a3770dc5ea
francium-96 <johntherippper96@gmail.com>
2023-12-08 16:31:43 +0000

    titan: Switch to AOSP Boot implementation

--- device.mk
+++ device.mk
@@ -58,8 +58,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-mtkimpl.recovery \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
     android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES += \
