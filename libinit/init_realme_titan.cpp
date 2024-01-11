5250379c626ae4cd3720b326c86e8332b030219d
francium-96 <johntherippper96@gmail.com>
2024-01-11 21:40:19 +0600

    titan: libinit: Add titan info

--- libinit/init_realme_titan.cpp
+++ libinit/init_realme_titan.cpp
@@ -10,6 +10,22 @@
 
 #include "vendor_init.h"
 
+static const variant_info_t titan_global_info = {
+    .hwc_value = "",
+    .sku_value = "titan",
+
+    .brand = "Realme",
+    .device = "titan",
+    .marketname = "Realme 10 Pro Plus 5G",
+    .model = "rmx3686",
+    .build_fingerprint = "realme/titan/titan:12/SP1A.210812.016/1695642825420:user/release-keys"
+};
+static const std::vector<variant_info_t> variants = {
+    tian_global_info,
+};
+
+
+
 void vendor_load_properties() {
     search_variant(variants);
 }
